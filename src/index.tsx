aa8793a82aa8308960e9e7adbd69dcba1b950806
dujinjin <dujinjin@duiba.com.cn>
2019-10-27 23:51:44 +0800

    add socket.io

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const socket = require('socket.io-client')('http://127.0.0.1:7001')
+
+socket.on('connect', () => {
+  console.log('connect!');
+//   socket.emit('res', 'hello world!');
+//   socket.emit('chat', 'hello world!')
+});
+
+socket.on('res', (msg:any) => {
+  console.log('res from server: %s!', msg)
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
